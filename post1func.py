9cffd606bc31b09e7e0c9b29188e2c14dfd2d027
JCYYTDD <70706344+JCYYTDD@users.noreply.github.com>
2022-12-04 22:40:04 +0800

    Add files via upload

--- post1func.py
+++ post1func.py
@@ -23,3 +23,25 @@ def post1(filepath):
         error_dialog = QtWidgets.QErrorMessage()
         error_dialog.showMessage('''Oh no!post函数出现问题，请检查一下您的url配置或者也有可能出现其他问题!
         ┭┮﹏┭┮''')
+
+
+# 该块儿仅为框架，不可直接拿走使用
+# 以下为重写的方法
+class NewQLineEdit(QtWidgets.QLineEdit):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAcceptDrops(True)  # 删除没有影响，目前不确定（因为True和False测试结果一样）
+        self.setDragEnabled(True)  # 删除没有影响，（因为True和False测试结果一样）
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():  # 当文件拖入此区域时为True
+            event.accept()  # 接受拖入文件
+        else:
+            event.ignore()  # 忽略拖入文件
+
+
+    def dropEvent(self, event):    # 本方法为父类方法，本方法中的event为鼠标放事件对象
+        urls = [u for u in event.mimeData().urls()]  # 范围文件路径的Qt内部类型对象列表，由于支持多个文件同时拖入所以使用列表存放
+        for url in urls:
+            self.setText(url.path()[1:])# 将Qt内部类型转换为字符串类型
